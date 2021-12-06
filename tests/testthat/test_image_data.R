241f6734bbeb68d43b7df5acf8efea79d2196019
Aaron Wolen <aaron@wolen.com>
2021-12-06 16:36:36 -0600

    Remove old tests

--- tests/testthat/test_image_data.R
+++
@@ -1,7 +0,0 @@
-dir_name <- "../testdata/visium"
-
-data_dir <- rprojroot::find_package_root_file("tests/testdata/visium")
-dest_dir <- file.path(tempdir(), "visium-array")
-
-Convert10x_Spatial_Tiledb(data_dir, dest = dest_dir, overwrite = TRUE)
-
