ed12cf45d24dc1087ca0a6805783caa151a010c0
Aaron Wolen <aaron@wolen.com>
2021-12-06 20:14:09 -0600

    Add class for image positions array

---
+++ tests/testthat/test_TiledbImagePositions.R
@@ -0,0 +1,14 @@
+data_dir <- rprojroot::find_package_root_file("tests/testdata/visium")
+spatial_dir <- file.path(data_dir, "spatial")
+pos_file <- file.path(spatial_dir, "tissue_positions_list.csv")
+
+tdb_uri <- file.path(tempdir(), "image-pos")
+
+teardown({
+  tiledb::tiledb_vfs_remove_dir(tdb_uri)
+})
+
+test_that("A TiledbImagePositions object can be created", {
+  tdb <<- TiledbImagePositions$new(tdb_uri, pos_file)
+  expect_true(inherits(tdb, "TiledbImagePositions"))
+})
