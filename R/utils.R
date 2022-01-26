41e5f1834f1ec098187ea70ff99c46b8fc8bdee6
Aaron Wolen <aaron@wolen.com>
2022-01-26 16:07:26 -0600

    Add helper for scalar characters

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,10 @@ is_named <- function(x) {
   !is.null(names(x))
 }
 
+is_scalar_character <- function(x) {
+  is.character(x) && length(x) == 1
+}
+
 string_starts_with <- function(x, prefix) {
   prefix <- paste0("^", prefix)
   grepl(prefix, x)
