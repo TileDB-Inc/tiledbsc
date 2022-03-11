540d781eabe715622e29b27686d474cba677f011
Aaron Wolen <aaron@wolen.com>
2022-03-11 06:09:17 -0600

    Reorder methods based on dependency

--- R/AnnotationPairwiseMatrixGroup.R
+++ R/AnnotationPairwiseMatrixGroup.R
@@ -10,6 +10,33 @@ AnnotationPairwiseMatrixGroup <- R6::R6Class(
 
   public = list(
 
+    #' @description Add a new [`AnnotationPairwiseMatrix`] array to the group.
+    #' @param data a [`matrix`] of annotation data to ingest. The `matrix` rows
+    #' must be aligned to the [`SCGroup`] dimension indicated by the group's
+    #' `dimension_name`.
+    #' @param name Name of the new pairwise annotation matrix.
+    #' @param metadata Named list of metadata to add.
+    add_matrix = function(data, name, metadata = NULL) {
+      if (missing(name)) {
+        stop("Must specify a `name` for the new AnnotationMatrix")
+      }
+      if (missing(data)) {
+        stop("Must provide a `matrix` to ingest into the new AnnotationMatrix")
+      }
+
+      # TODO: Verify that the matrix is aligned to the group's dimension
+      # create the new array
+      array_uri <- file.path(self$uri, name)
+      array <- AnnotationPairwiseMatrix$new(
+        uri = array_uri,
+        verbose = self$verbose
+      )
+
+      index_cols <- paste(self$dimension_name, c("i", "j"), sep = "_")
+      array$from_matrix(data, index_cols)
+      if (!is.null(metadata)) array$add_metadata(metadata)
+      self$arrays[[name]] <- array
+    },
 
     #' @description Convert a [`SeuratObject::Graph`] object to
     #' [`AnnotationPairwiseMatrix`].
@@ -45,34 +72,6 @@ AnnotationPairwiseMatrixGroup <- R6::R6Class(
           graph_technique = technique
         )
       )
-    },
-
-    #' @description Add a new [`AnnotationPairwiseMatrix`] array to the group.
-    #' @param data a [`matrix`] of annotation data to ingest. The `matrix` rows
-    #' must be aligned to the [`SCGroup`] dimension indicated by the group's
-    #' `dimension_name`.
-    #' @param name Name of the new pairwise annotation matrix.
-    #' @param metadata Named list of metadata to add.
-    add_matrix = function(data, name, metadata = NULL) {
-      if (missing(name)) {
-        stop("Must specify a `name` for the new AnnotationMatrix")
-      }
-      if (missing(data)) {
-        stop("Must provide a `matrix` to ingest into the new AnnotationMatrix")
-      }
-
-      # TODO: Verify that the matrix is aligned to the group's dimension
-      # create the new array
-      array_uri <- file.path(self$uri, name)
-      array <- AnnotationPairwiseMatrix$new(
-        uri = array_uri,
-        verbose = self$verbose
-      )
-
-      index_cols <- paste(self$dimension_name, c("i", "j"), sep = "_")
-      array$from_matrix(data, index_cols)
-      if (!is.null(metadata)) array$add_metadata(metadata)
-      self$arrays[[name]] <- array
     }
   ),
 
