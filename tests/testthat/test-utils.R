58f51f656a550619a0a933b64f8b3191dacebbcb
Aaron Wolen <aaron@wolen.com>
2022-03-19 14:52:50 -0500

    Consolidate util tests

--- tests/testthat/test-utils.R
+++
@@ -1,9 +0,0 @@
-test_that("renaming works", {
-  vec1 <- c(a = 1, b = 2, c = 3)
-  vec2 <- rename(vec1, c(A = "a", C = "c"))
-  expect_identical(names(vec2), c("A", "b", "C"))
-  expect_error(
-    rename(vec1, c(A = "notpresent")),
-    "All 'names' must be in 'x'"
-  )
-})
--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -18,3 +18,13 @@ test_that("matrices with empty dimensions are detected", {
     has_dimnames(Matrix::Matrix(1, dimnames = list("A", "B")))
   )
 })
+
+test_that("vector renaming works", {
+  vec1 <- c(a = 1, b = 2, c = 3)
+  vec2 <- rename(vec1, c(A = "a", C = "c"))
+  expect_identical(names(vec2), c("A", "b", "C"))
+  expect_error(
+    rename(vec1, c(A = "notpresent")),
+    "All 'names' must be in 'x'"
+  )
+})
