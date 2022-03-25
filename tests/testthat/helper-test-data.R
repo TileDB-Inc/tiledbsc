7d4f86b5d29d1103493c86784b21ffdf12ff0d3d
Aaron Wolen <aaron@wolen.com>
2022-03-25 10:43:06 -0500

    Drop test helpers only used to test old classes

--- tests/testthat/helper-test-data.R
+++ tests/testthat/helper-test-data.R
@@ -1,12 +1,2 @@
 # Load pbmc_small from SeuratObject
 data("pbmc_small", package = "SeuratObject")
-
-# Locate test files
-# data_dir <- testthat::test_path("testdata", "visium")
-data_dir <- system.file("tests/testdata", package = "tiledbsc")
-spatial_dir <- file.path(data_dir, "spatial")
-
-h5_file <- file.path(data_dir, "filtered_feature_bc_matrix.h5")
-img_file <- file.path(spatial_dir, "tissue_lowres_image.png")
-sf_file <- file.path(spatial_dir, "scalefactors_json.json")
-pos_file <- file.path(spatial_dir, "tissue_positions_list.csv")
