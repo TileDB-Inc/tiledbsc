8715e6f462f1d34749e0e0dd3f536e7413bde12b
Aaron Wolen <aaron@wolen.com>
2022-03-25 13:05:07 -0500

    Add package doc

---
+++ R/tiledbsc-package.R
@@ -0,0 +1,14 @@
+#' @description tiledbsc provides an implementation of the [Unified Single-cell
+#' Data Model and API](https://github.com/single-cell-data/matrix-api) for
+#' storing and querying single-cell data using TileDB.
+#'
+#' The API is designed around R6-based classes corresponding to the components
+#' described in the [data model
+#' specification](https://github.com/single-cell-data/matrix-api/blob/main/specification.md).
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
