3a9b312e04b069f8b70977a8ceecdd30bf2a940e
Aaron Wolen <aaron@wolen.com>
2022-05-11 14:06:08 -0500

    Add util for detecting remote uris

--- R/utils.R
+++ R/utils.R
@@ -87,15 +87,14 @@ check_package <- function(package) {
   stop(paste0("Package '", package, "' must be installed"))
 }
 
+is_remote_uri <- function(x) {
+  string_starts_with(x, "s3://") | string_starts_with(x, "tiledb://")
+}
+
 # Drop-in replacement for file.paths() that ignores the platform separator when
 # constructing remote S3 or TileDB URIs
 file_path <- function(..., fsep = .Platform$file.sep) {
   paths <- list(...)
-  if (
-    string_starts_with(paths[[1]], "s3://") ||
-    string_starts_with(paths[[1]], "tiledb://")
-  ) {
-    fsep <- "/"
-  }
+  if (is_remote_uri(paths[[1]])) fsep <- "/"
   file.path(..., fsep = fsep)
 }
