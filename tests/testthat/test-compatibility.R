5ef35775575a55b9c79b2fca57402a06a701997b
Aaron Wolen <aaron@wolen.com>
2022-05-26 14:55:34 -0500

    Add scgroup backwards compatibility tests

--- tests/testthat/test-compatibility.R
+++ tests/testthat/test-compatibility.R
@@ -16,3 +16,30 @@ test_that("compatibility is maintained with SCDataset created using v0.1.2", {
   # obs dataframe manually added to the SCGroup misc group is present
   expect_identical(scdataset$members$RNA$misc$count_members(), 1)
 })
+
+test_that("compatibility is maintained with v0.1.2 SCGroup", {
+  uri <- testthat::test_path("test-data/scdataset-pbmc-small_0-1-2/scgroup_RNA")
+  expect_warning(
+    scgroup <- SCGroup$new(uri, verbose = FALSE)
+  )
+  expect_true(inherits(scgroup, "SCGroup"))
+
+  # verify uns wasn't created in the presence of an existing misc group
+  expect_false(dir.exists(file.path(scgroup$uri, "uns")))
+
+  # obs dataframe manually added to the SCGroup misc group is present
+  expect_identical(
+    suppressWarnings(scgroup$misc$count_members()),
+    1
+  )
+
+  # misc is an alias for uns
+  expect_identical(
+    suppressWarnings(scgroup$misc$uri),
+    scgroup$uns$uri
+  )
+
+  expect_silent(
+    pbmc_small_assay2 <- scgroup$to_seurat_assay()
+  )
+})
