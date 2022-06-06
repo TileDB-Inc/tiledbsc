eb59aaeaa266255368788dfb3fe53d34a167418b
Aaron Wolen <aaron@wolen.com>
2022-06-06 10:39:18 -0500

    Add tests for basic mechanics of array dim slicing

--- tests/testthat/test_TileDBArray.R
+++ tests/testthat/test_TileDBArray.R
@@ -28,9 +28,43 @@ test_that("TileDBArray helper functions", {
   attr_cols <- setdiff(colnames(df), index_cols)
   expect_identical(tdb$attrnames(), attr_cols)
 
+  # metadata
   md <- list(baz = "qux", foo = "bar")
   tdb$add_metadata(md)
   expect_equal(tdb$get_metadata(key = "foo"), "bar")
   expect_equal(tdb$get_metadata(prefix = "foo"), md["foo"])
   expect_equal(tdb$get_metadata(), md)
+
+  # dimension slicing
+  tdb <- TileDBArray$new(uri = uri, verbose = TRUE)
+  expect_error(
+    tdb$set_query(),
+    "Must specify at least one dimension to slice"
+  )
+  expect_error(
+    tdb$set_query(dim = "foo"),
+    "'dims' must be a named list of character vectors"
+  )
+  expect_error(
+    tdb$set_query(dim = "foo"),
+    "'dims' must be a named list of character vectors"
+  )
+expect_error(
+    tdb$set_query(dim = list(a = 1L)),
+    "'dims' must be a named list of character vectors"
+  )
+  expect_error(
+    tdb$set_query(dim = list(foo = "bar")),
+    "All 'dims' element names must match an array dimension"
+  )
+
+  expect_silent(
+    tdb$set_query(dim = list(Dept = c("A", "B")))
+  )
+
+  # verify selected ranges were set
+  expect_equal(
+    selected_ranges(tdb$object),
+    list(Dept = cbind(c("A", "B"), c("A", "B")))
+  )
 })
