bbdf56e54f29415080ec7e2d0e5e193dd0348ad0
Aaron Wolen <aaron@wolen.com>
2022-06-08 11:02:13 -0500

    Switch empty test arrays to string dims

--- tests/testthat/helper-arrays.R
+++ tests/testthat/helper-arrays.R
@@ -1,6 +1,7 @@
 create_empty_test_array <- function(uri) {
   stopifnot(!dir.exists(uri))
-  dom <- tiledb::tiledb_domain(dims = tiledb::tiledb_dim("d0", domain = 0L:1L))
+  dim <- tiledb::tiledb_dim("d0", type = "ASCII", domain = NULL, tile = NULL)
+  dom <- tiledb::tiledb_domain(dims = dim)
   schema <- tiledb::tiledb_array_schema(
     domain = dom,
     attrs = c(tiledb::tiledb_attr("a", type = "INT32")),
