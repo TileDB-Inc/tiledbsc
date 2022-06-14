2b5fa006f0d99f4422d697d84bb20699b89746d2
Aaron Wolen <aaron@wolen.com>
2022-06-14 16:17:06 -0500

    Fix logic for setting attribute filters on TileDBArray queries

--- R/TileDBArray.R
+++ R/TileDBArray.R
@@ -174,11 +174,11 @@ TileDBArray <- R6::R6Class(
       # check if attr_filter is NULL whether set_query was called directly from
       # a TileDBArray or indirectly via AnnotationGroup
       attr_filter_is_null <- (
-        !is.null(substitute(attr_filter)) &&
-        substitute(attr_filter) != "NULL"
+        is.null(substitute(attr_filter)) ||
+        substitute(attr_filter) == "NULL"
       )
 
-      if (attr_filter_is_null) {
+      if (!attr_filter_is_null) {
         # if the attr_filter was passed in from AnnotationGroup$set_query()
         # then subsitute() will return a `name` instead of a `call` because
         # the expression has been converted to a character vector and needs
