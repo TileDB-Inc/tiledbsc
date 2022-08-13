bff883683ddd191a39a49bfa3bb9da65407d3ff8
Aaron Wolen <aaron@wolen.com>
2022-08-13 16:11:32 -0500

    Update assay_matrix_apply() arguments for specifying slices

--- R/utils-tiledbcloud.R
+++ R/utils-tiledbcloud.R
@@ -1,13 +1,15 @@
-#' Apply a function to AssayMatrix data
+#' Apply a function to an AssayMatrix slice
 #'
-#' Intended to be called as a UDF on TileDB Cloud. tiledbsc isn't
-#' available in the UDF environment so some functions must be reused.
+#' Primarily intended to be called as a UDF on TileDB Cloud.
+#'
 #' @param uri Must point to an AssayMatrix array
 #' @param fun The callback function to apply to the matrix
+#' @param dims a named list of character vectors. Each name must correspond
+#' to an array dimension.
 #' @examples
 #'
 #' @noRd
-assay_matrix_apply <- function(uri, fun = identity, obs_ids = NULL, var_ids = NULL) {
+assay_matrix_apply <- function(uri, fun = identity, dims = NULL) {
   library(Matrix)
   library(tiledb)
 
@@ -19,11 +21,18 @@ assay_matrix_apply <- function(uri, fun = identity, obs_ids = NULL, var_ids = NU
     message(dots)
   }
 
-  # assemble per-dimension selected ranges
-  dims <- modifyList(list(), list(obs_id = obs_ids, var_id = var_ids))
-  if (length(dims) == 0) stop("Cannot proceed with empty selected ranges")
-  dims <- lapply(dims, sort)
-  ranges <- lapply(dims, function(x) unname(cbind(x, x)))
+  # largely taken from TileDBArray$set_query()
+  stopifnot(
+    "'dims' must be a named list of character vectors" =
+      is_named_list(dims) && all(vapply_lgl(dims, is_character_or_null))
+  )
+
+  # list of dimensions to slice discarding NULL elements
+  dims <- modifyList(list(), dims)
+
+  # Convert each dim vector to a two-column matrix where each row
+  # describes one pair of minimum and maximum values.
+  ranges <- lapply(X = dims, FUN = function(x) unname(cbind(x, x)))
 
   log_message(sprintf("Opening array %s", uri))
   tdb <- tiledb_array(uri, return_as = "data.frame", selected_ranges = ranges)
